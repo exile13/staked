4c5a5eec66ee33f90cbc20bb6e4af794ee0a8ab9
blackjok3r <blackjok3r@gmail.com>
2018-10-09 23:16:06 +0800

    komodo missing

--- install/buildkomodo.sh
+++ install/buildkomodo.sh
@@ -1,5 +1,5 @@
 #Install Deps
-mkdir -p $HOME/staked/master
+mkdir -p $HOME/staked/komodo/master
 sudo apt-get update
 sudo apt-get -y install build-essential pkg-config libc6-dev m4 g++-multilib autoconf libtool ncurses-dev unzip git python python-zmq zlib1g-dev wget libcurl4-openssl-dev bsdmainutils automake curl tmux htop slurm bc jq
 #Install Komodo
