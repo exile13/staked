7345522f90abb0219c0780aa1c6438be67983b29
blackjok3rtt <30971146+blackjok3rtt@users.noreply.github.com>
2018-10-16 10:34:03 +0800

    remove komodo.conf generation

--- install/buildkomodo.sh
+++ install/buildkomodo.sh
@@ -12,15 +12,5 @@ localrev=$(git rev-parse HEAD)
 echo $localrev > $HOME/staked/komodo/master/lastbuildcommit
 mv src/komodod $HOME/staked/komodo/master
 mv src/komodo-cli $HOME/staked/komodo/master
-cd ~
-mkdir .komodo
-cd .komodo
-touch komodo.conf
-echo "rpcuser=user`head -c 32 /dev/urandom | base64`" > komodo.conf
-echo "rpcpassword=password`head -c 32 /dev/urandom | base64`" >> komodo.conf
-echo "daemon=1" >> komodo.conf
-echo "server=1" >> komodo.conf
-echo "txindex=1" >> komodo.conf
-chmod 0600 komodo.conf
 sudo ln -sf $HOME/staked/asset-cli /usr/local/bin/asset-cli
 sudo ln -sf $HOME/staked/komodo/master/komodo-cli /usr/local/bin/staked-cli
